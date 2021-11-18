9ac778d152eeff628e55a3cc3778ef46047f054b
Manuel <manuel.sifuentes17@tectijuana.edu.mx>
2021-11-18 13:34:34 -0800

    uploading evaluation 02

--- Unit_2/Evaluation/Evaluation.scala
+++ Unit_2/Evaluation/Evaluation.scala
@@ -1,53 +1,63 @@
-// Answer the following questions with Spark DataFrames and Scala using the "CSV"
-// Netflix_2011_2016.csv found in the spark-dataframes folder.
+//1. Load into a dataframe Iris.csv
+val iris_df=spark.read.format("csv").option("header","true").load("iris.csv")
 
-// 1. Start a simple Spark session.
-import org.apache.spark.sql.SparkSession
-val spark = SparkSession.builder().getOrCreate()
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+//import org.apache.spark.sql.types.DoubleType
 
-// 2. Upload Netflix Stock CSV file, have Spark infer the data types.
-val df = spark.read.option("header", "true").option("inferSchema","true")csv("Netflix_2011_2016.csv") 
+val data = iris_df.withColumn("sepal_length", $"sepal_length".cast(DoubleType)).withColumn("sepal_width", $"sepal_width".cast(DoubleType)).withColumn("petal_length", $"petal_length".cast(DoubleType)).withColumn("petal_width", $"petal_width".cast(DoubleType))
 
-// 3. What are the names of the columns?
-df.columns
+//2. What are the names of the columns?
+data.columns
 
-// 4. What is the scheme like?
-df.printSchema()
+//3. What is the scheme like?
+data.printSchema()
 
-// 5. Print the first 5 columns.
-for(row <- df.head(5)){
-    println(row)
-}
+//4. Print the first 5 columns.
+data.show(5)
 
-// 6. Use describe () to learn about the DataFrame.
-df.describe().show()
+//5. Use the describe () method to learn more about the data in the DataFrame.
+data.describe().show()
 
-// 7. Create a new dataframe with a new column called “HV Ratio” which is the relationship between the price in the “High” column versus the “Volume” column of shares traded for a day. Hint is an operation
-val df2 = df.withColumn("HV Ratio",df("High")/df("Volume"))
+//6. Make the pertinent transformation for the categorical data which will be our labels to be classified.
+import org.apache.spark.ml.feature.VectorAssembler
+val assembler = new VectorAssembler().setInputCols(Array("sepal_length", "sepal_width", "petal_length", "petal_width")).setOutputCol("features")
+val features = assembler.transform(data)
+features.show(5)
 
-// 8. What day had the highest peak in the “Open” column?
-df.orderBy($"Open".desc).show(1)
+import org.apache.spark.ml.feature.StringIndexer
+val labelIndexer = new StringIndexer().setInputCol("species").setOutputCol("indexedLabel").fit(features)
+println(s"Found labels: ${labelIndexer.labels.mkString("[", ", ", "]")}")
 
-// 9. What is the meaning of the Close column “Close” in the context of financial information, explain it, there is nothing to code?
-// It is the price at which the stock sales ended on the respective day.
+import org.apache.spark.ml.feature.VectorIndexer
+val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(features)
 
-// 10. What is the maximum and minimum in the “Volume” column?
-df.select(max("Volume")).show()
-df.select(min("Volume")).show()
+val splits = features.randomSplit(Array(0.6, 0.4))
+val trainingData = splits(0)
+val testData = splits(1)
 
-//11. With Scala / Spark $ Syntax answer the following:
+val layers = Array[Int](4, 5, 5, 3)
 
-// a. How many days was the “Close” column less than $ 600?
-df.filter($"Close"<600).count()
-// b. What percentage of the time was the “High” column greater than $ 500?
-(df.filter($"High">500).count()*1.0/df.count())*100
-// c. What is the Pearson correlation between column "High" and column "Volume"?
-df.select(corr("High","Volume")).show()
-// d. What is the maximum in the “High” column per year?
-val df_year = df.withColumn("Year",year(df("Date")))
-val df_max = df_year.select($"Year",$"High").groupBy("Year").max()
-df_max.select($"Year",$"max(High)").show()
-// e. What is the average “Close” column for each calendar month?
-val df_month = df.withColumn("Month",month(df("Date")))
-val month_avgs = df_month.select($"Month",$"Close").groupBy("Month").mean()
-month_avgs.select($"Month",$"avg(Close)").show()
+//7. Build the classification model and explain its architecture.
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setBlockSize(128).setSeed(System.currentTimeMillis).setMaxIter(200)
+
+import org.apache.spark.ml.feature.IndexToString
+val labelConverter = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labels)
+
+import org.apache.spark.ml.Pipeline
+val pipeline = new Pipeline().setStages(Array(labelIndexer, featureIndexer, trainer, labelConverter))
+
+val model = pipeline.fit(trainingData)
+
+//8. Print the model results
+val predictions = model.transform(testData)
+
+predictions.show(5)
+
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+
+val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
+val accuracy = evaluator.evaluate(predictions)
+println("Test Error = " + (1.0 - accuracy))
+
